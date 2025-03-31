42b351e3775ead50bed24a6159f0fbd44509795a
Thiago Felipe <thiagof.g3@gmail.com>
2025-03-31 07:58:39 -0300

    refactor: deletando acessos set publicos da classe Category - não são necessários e podem gerar problemas de segurança pois a classe deve ser responsável pelo seu comportamento

--- domain/src/main/java/com/admin/menu/movie/domain/category/Category.java
+++ domain/src/main/java/com/admin/menu/movie/domain/category/Category.java
@@ -75,39 +75,19 @@ public class Category extends AggregateRoot<CategoryID> {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public boolean isActive() {
         return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
     }
 
     public Instant getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }
-
     public Instant getUpdatedAt() {
         return updatedAt;
     }
 
-    public void setUpdatedAt(Instant updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
     public Instant getDeletedAt() {
         return deletedAt;
-    }
-
-    public void setDeletedAt(Instant deletedAt) {
-        this.deletedAt = deletedAt;
     }
 }
