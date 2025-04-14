51e936de63416498de64f91d9f64046f64224226
Thiago Felipe <thiagof.g3@gmail.com>
2025-04-14 08:47:18 -0300

    feat: criando caso de uso Default para a criacao da Category

--- application/src/main/java/com/admin/menu/movie/application/category/create/DefaultCreateCategoryUseCase.java
+++ application/src/main/java/com/admin/menu/movie/application/category/create/DefaultCreateCategoryUseCase.java
@@ -1,4 +1,28 @@
 package com.admin.menu.movie.application.category.create;
 
-public class DefaultCreateCategoryUseCase {
+import com.admin.menu.movie.domain.category.Category;
+import com.admin.menu.movie.domain.category.CategoryGateway;
+import com.admin.menu.movie.domain.validation.handler.ThrowsValidationHandler;
+
+import java.util.Objects;
+
+public class DefaultCreateCategoryUseCase extends CreateCategoryUseCase {
+
+	private final CategoryGateway categoryGateway;
+
+	public DefaultCreateCategoryUseCase(final CategoryGateway categoryGateway) {
+		this.categoryGateway = Objects.requireNonNull(categoryGateway);
+	}
+
+	@Override
+	public CreateCategoryOutput execute(final CreateCategoryCommand command) {
+		final var category = Category.newCategory(
+				command.name(),
+				command.description(),
+				command.isActive()
+		);
+		category.validate(new ThrowsValidationHandler());
+
+		return CreateCategoryOutput.from(this.categoryGateway.create(category));
+	}
 }
