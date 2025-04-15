44e387655c94c7b80bd8c17c4a8ecc9713db9bec
Thiago Felipe <thiagof.g3@gmail.com>
2025-04-15 08:04:46 -0300

    test: deveria disparar um erro do tipo DomainException quando passado um parametro invalido

--- application/src/test/java/com/admin/menu/movie/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/com/admin/menu/movie/application/category/create/CreateCategoryUseCaseTest.java
@@ -1,9 +1,14 @@
 package com.admin.menu.movie.application.category.create;
 
 import com.admin.menu.movie.domain.category.CategoryGateway;
+import com.admin.menu.movie.domain.exceptions.DomainException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Objects;
 
@@ -13,7 +18,15 @@ import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class CreateCategoryUseCaseTest {
+
+	@InjectMocks
+	private DefaultCreateCategoryUseCase useCase;
+
+	@Mock
+	private CategoryGateway categoryGateway;
+
 	@Test
 	public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() {
 		final var expectedName = "Any Movie";
@@ -26,11 +39,8 @@ public class CreateCategoryUseCaseTest {
 		 * dos dados que esta sendo informado na entrada.
 		 */
 		final var command = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
-		final CategoryGateway categoryGateway = Mockito.mock(CategoryGateway.class);
 
 		when(categoryGateway.create(any())).thenAnswer(returnsFirstArg());
-
-		final var useCase = new DefaultCreateCategoryUseCase(categoryGateway);
 
 		final var actualOutput = useCase.execute(command);
 
@@ -47,5 +57,22 @@ public class CreateCategoryUseCaseTest {
 							&& Objects.nonNull(category.getUpdatedAt())
 							&& Objects.isNull(category.getDeletedAt());
 				}));
+	}
+
+	@Test
+	public void givenAInvalidCommand_whenCallsCreateCategory_shouldReturnDomainException() {
+		final String expectedName = null;
+		final var expectedDescription = "Any description";
+		final var expectedIsActive = true;
+		final var expectedErrorMessage = "'name' should not be null";
+		final var expectedErrorCount = 0;
+
+		final var command = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+		final var actualError = Assertions.assertThrows(DomainException.class, () -> useCase.execute(command));
+
+		Assertions.assertEquals(expectedErrorMessage, actualError.getMessage());
+
+		Mockito.verify(categoryGateway, times(expectedErrorCount)).create(any());
 	}
 }
