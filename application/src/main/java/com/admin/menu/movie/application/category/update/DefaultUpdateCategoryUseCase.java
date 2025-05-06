8e348bdd9d5456440256e27bc7bb7a2f6521228f
Thiago Felipe <thiagof.g3@gmail.com>
2025-05-06 10:02:49 -0300

    feat: criando o caso de uso padrão para atualizacao de uma Categoria

--- application/src/main/java/com/admin/menu/movie/application/category/update/DefaultUpdateCategoryUseCase.java
+++ application/src/main/java/com/admin/menu/movie/application/category/update/DefaultUpdateCategoryUseCase.java
@@ -1,4 +1,48 @@
 package com.admin.menu.movie.application.category.update;
 
-public class DefaultUpdateCategoryUseCase {
+import com.admin.menu.movie.domain.category.Category;
+import com.admin.menu.movie.domain.category.CategoryGateway;
+import com.admin.menu.movie.domain.category.CategoryID;
+import com.admin.menu.movie.domain.exceptions.DomainException;
+import com.admin.menu.movie.domain.validation.Error;
+import com.admin.menu.movie.domain.validation.handler.Notification;
+import io.vavr.API;
+import io.vavr.control.Either;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class DefaultUpdateCategoryUseCase extends UpdateCategoryUseCase {
+
+	private final CategoryGateway categoryGateway;
+
+	public DefaultUpdateCategoryUseCase(final CategoryGateway categoryGateway) {
+		this.categoryGateway = Objects.requireNonNull(categoryGateway);
+	}
+
+	@Override
+	public Either<Notification, UpdateCategoryOutput> execute(UpdateCategoryCommand command) {
+		final CategoryID id = CategoryID.from(command.id());
+		final var name = command.name();
+		final var description = command.description();
+		final var isActive = command.isActive();
+
+		final Category category = this.categoryGateway.findById(id)
+				.orElseThrow(notFound(id));
+		final Notification notification = Notification.create();
+
+		category.update(name, description, isActive).validate(notification);
+
+		return notification.hasError() ? API.Left(notification) : update(category);
+	}
+
+	private Either<Notification, UpdateCategoryOutput> update(Category category) {
+		return API.Try(() -> this.categoryGateway.update(category))
+				.toEither()
+				.bimap(Notification::create, UpdateCategoryOutput::from);
+	}
+
+	private static Supplier<DomainException> notFound(final CategoryID id) {
+		return () -> DomainException.with(new Error("Category with ID %s was not found".formatted(id.getValue())));
+	}
 }
