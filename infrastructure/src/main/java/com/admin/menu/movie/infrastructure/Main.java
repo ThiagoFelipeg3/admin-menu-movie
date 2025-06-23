53a7c1ec7bedac3e670c5d7b190ca2dc92b4cd4a
Thiago Felipe <thiagof.g3@gmail.com>
2025-06-23 09:14:39 -0300

    feat: configurando o spring no pacote infrastructure como uma webServer

--- infrastructure/src/main/java/com/admin/menu/movie/infrastructure/Main.java
+++ infrastructure/src/main/java/com/admin/menu/movie/infrastructure/Main.java
@@ -1,7 +1,13 @@
 package com.admin.menu.movie.infrastructure;
 
+import com.admin.menu.movie.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Init!");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ infrastructure/src/main/java/com/admin/menu/movie/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.admin.menu.movie.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.admin.menu.movie")
+public class WebServerConfig {
+}
