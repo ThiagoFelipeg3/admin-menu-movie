0306581db5601c749c109d5926ebc129a14bc90f
Thiago Felipe <thiagof.g3@gmail.com>
2025-12-20 17:56:18 -0300

    feat: criando a classe gateway CategoryMySQLGateway sem implementacao

--- infrastructure/src/main/java/com/admin/menu/movie/infrastructure/category/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/com/admin/menu/movie/infrastructure/category/CategoryMySQLGateway.java
@@ -1,4 +1,46 @@
-package com.admin.menu.movie.infrastructure.category.pesistence;
+package com.admin.menu.movie.infrastructure.category;
 
-public class CategoryMySQLGateway {
+import com.admin.menu.movie.domain.category.Category;
+import com.admin.menu.movie.domain.category.CategoryGateway;
+import com.admin.menu.movie.domain.category.CategoryID;
+import com.admin.menu.movie.domain.category.CategorySearchQuery;
+import com.admin.menu.movie.domain.pagination.Pagination;
+import com.admin.menu.movie.infrastructure.category.pesistence.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoryMySQLGateway implements CategoryGateway {
+
+	private final CategoryRepository repository;
+
+	public CategoryMySQLGateway(final CategoryRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public Category create(Category category) {
+		return null;
+	}
+
+	@Override
+	public void deleteById(CategoryID id) {
+
+	}
+
+	@Override
+	public Optional<Category> findById(CategoryID id) {
+		return Optional.empty();
+	}
+
+	@Override
+	public Category update(Category category) {
+		return null;
+	}
+
+	@Override
+	public Pagination<Category> findAll(CategorySearchQuery query) {
+		return null;
+	}
 }
