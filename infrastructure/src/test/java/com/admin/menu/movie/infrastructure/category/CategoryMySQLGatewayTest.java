dced022dba5f63510568b4ea5729fd8343b5eb53
Thiago Felipe <thiagof.g3@gmail.com>
2025-12-20 17:56:59 -0300

    test: criando a base para os testes no CategoryMySQLGateway

--- infrastructure/src/test/java/com/admin/menu/movie/infrastructure/category/CategoryMySQLGatewayTest.java
+++ infrastructure/src/test/java/com/admin/menu/movie/infrastructure/category/CategoryMySQLGatewayTest.java
@@ -1,4 +1,23 @@
 package com.admin.menu.movie.infrastructure.category;
 
+import com.admin.menu.movie.infrastructure.MySQLGatewayTest;
+import com.admin.menu.movie.infrastructure.category.pesistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@MySQLGatewayTest
 public class CategoryMySQLGatewayTest {
+
+	@Autowired
+	private CategoryMySQLGateway categoryMySQLGateway;
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	@Test
+	public void testInjectedDependencies() {
+		Assertions.assertNotNull(categoryMySQLGateway);
+		Assertions.assertNotNull(categoryRepository);
+	}
 }
